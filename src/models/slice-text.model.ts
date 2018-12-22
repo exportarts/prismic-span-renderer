6e1ad59a884ea87849b8007f76b58b07f6ae5aad
Enzo Volkmann <enzo@exportarts.io>
2018-12-22 14:46:34 +0100

    Using single quotes

--- src/models/slice-text.model.ts
+++ src/models/slice-text.model.ts
@@ -2,16 +2,16 @@ import { Span } from './span.model';
 
 export interface Paragraph {
   type:
-    | "paragraph"
-    | "preformatted"
-    | "heading1"
-    | "heading2"
-    | "heading3"
-    | "heading4"
-    | "heading5"
-    | "heading6"
-    | "o-list-item"
-    | "list-item";
+    | 'paragraph'
+    | 'preformatted'
+    | 'heading1'
+    | 'heading2'
+    | 'heading3'
+    | 'heading4'
+    | 'heading5'
+    | 'heading6'
+    | 'o-list-item'
+    | 'list-item';
   text: string;
   spans: Span[];
 }
