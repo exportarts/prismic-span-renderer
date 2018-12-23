1e5f4a9706c9cfb6a2698a981e952d444cd044a6
Enzo Volkmann <enzo@exportarts.io>
2018-12-23 20:08:20 +0100

    fix(api): Added missing export

--- src/public_api.ts
+++ src/public_api.ts
@@ -4,6 +4,7 @@ export { renderText } from './renderer/text';
 export { renderImage, ImageConfig } from './renderer/image';
 
 export * from './models/api.model';
+export * from './models/slice.model';
 export * from './models/image.model';
 export * from './models/paragraph.model';
 export * from './models/span.model';
