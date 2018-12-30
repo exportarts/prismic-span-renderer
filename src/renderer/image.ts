a67e9ecf449f244a47dcc758d5fa794b932ff97c
Enzo Volkmann <enzo@exportarts.io>
2018-12-30 16:14:44 +0100

    feat(renderer): Added image alt and title

--- src/renderer/image.ts
+++ src/renderer/image.ts
@@ -15,6 +15,29 @@ export interface ImageConfig {
    * ```
    */
   imgClass?: string;
+  /**
+   * "alt"-text for <img>.
+   * 
+   * This text is used instead of the alt-text delivered
+   * by prismic.
+   * 
+   * @example
+   * ```html
+   * <!-- alt: 'image of something' -->
+   * <img alt="image of something" src="...">
+   * ```
+   */
+  alt?: string;
+  /**
+   * "title"-text for <img>.
+   * 
+   * @example
+   * ```html
+   * <!-- title: 'this is an image' -->
+   * <img title="this is an image" src="...">
+   * ```
+   */
+  title?: string;
 }
 
 /**
@@ -25,8 +48,15 @@ export interface ImageConfig {
  * @param config configuration to render this image
  */
 export function renderImage(config: ImageConfig): string {
+  if (config.alt) {
+    config.image.alt = config.alt;
+  }
+
   const imgClass = config.imgClass ? `class="${config.imgClass}"` : '';
-  const img = `<img ${imgClass} src="${config.image.url}">`;
+  const imgAlt = config.image.alt ? `alt="${config.image.alt}"` : '';
+  const imgTitle = config.title ? `title="${config.title}"` : '';
+  const imgSrc = `src="${config.image.url}"`;
+  const img = `<img ${imgClass} ${imgAlt} ${imgTitle} ${imgSrc}>`;
 
   if (config.caption) {
     return `
